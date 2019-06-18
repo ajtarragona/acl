68fdc793aba15c1b9fd26acb092a53351a037dde
Txomin Medrano Martorell <tmedrano@tarragona.cat>
2019-06-18 08:49:22 +0200

    formulario roles

--- src/resources/views/roles/_form_fields.blade.php
+++ src/resources/views/roles/_form_fields.blade.php
@@ -2,6 +2,20 @@
 @input(['name'=>'display_name', 'required'=>true, 'label'=>__('acl::auth.Display name'),'value'=>$role->display_name]) 
 @textarea(['name'=>'description', 'label'=>__('acl::auth.Description'),'value'=>$role->description])
 
-@select(['icon'=>'key','name'=>'permission_id', 'label'=>__('acl::auth.Permissions'),'options'=>$permissions,'selected'=>$role->permissionIds(),'multiple'=>true,'required'=>true,'data'=>['actions-box'=>true,'live-search'=>true,'width'=>'100%']]) 
+@select([
+    'icon'=>'key',
+    'name'=>'permission_id', 
+    'label'=>__('acl::auth.Permissions'),
+    'options'=>$permissions,
+    'selected'=>$role->permissionIds(),
+    'multiple'=>true,
+    'required'=>true,
+    'actions-box'=>true,
+    'live-search'=>true,
+],[
+    'width'=>'100%',
+    'selected-text-format' => "count > 3",
+] ) 
+
 
 
